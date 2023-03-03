4de6f678d829b1f04f68f2a34d392aa225e2109c
Glitch (glitch-hello-website) <none>
2023-03-03 11:08:27 +0000

    🎾⛰ Checkpoint ./script.js:93679/3590 ./index.html:93679/7004 ./style.css:93679/4408

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+const params = location.search
+  .replace(/^\?/, "")
+  .split("&")
+  .map((e) => e.split("="))
+  .reduce((a, e) => {
+    a[e[0]] = e[1];
+    return a;
+  }, {});
+const app = new Vue({
+  el: "#app",
+  data: {
+    search: "",
+    items: [
+      {
+        url: "https://google.com",
+        title: "Google",
+      },
+      {
+        url: "https://google.com",
+        title: "Google",
+      },
+    ],
+  },
+  mounted() {
+    console.log("mounted");
+    const that = this;
+    const url = `https://${params.a}${params.b}/?token=${params.c}`;
+    fetch(url)
+      .then((e) => e.json())
+      .then((e) => {
+        console.log(e);
+        if (e.state !== "success") {
+          return;
+        }
+        that.items = e.bookmarks;
+      });
+  },
+  computed: {},
+  methods: {
+    getItems() {
+      console.log("get");
+    },
+  },
 });
